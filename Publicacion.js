122f5e38a6c42fabebe8a4566980c1bed2bf9130
tlato-tlatoani <clarisseowo@gmail.com>
2025-09-17 16:12:18 -0600

    Add files via upload

---
+++ Publicacion.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+  fetch("SidebarUsuario.html")
+    .then(response => response.text())
+    .then(data => {
+      document.getElementById("sidebar-container").innerHTML = data;
+    });
+});
+
---
+++ SidebarAdmin.js
@@ -0,0 +1,7 @@
+/*document.addEventListener("DOMContentLoaded", () => {
+  fetch("SidebarAdmin.html")
+    .then(response => response.text())
+    .then(data => {
+      document.body.insertAdjacentHTML("afterbegin", data);
+    });
+});*/
